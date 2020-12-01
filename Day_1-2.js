0fdc9a9edda230e89696b514629e40df7b414a07
ada-kuhn <adam.kuhn20@gmail.com>
2020-12-01 22:48:39 +0200

    minor improvemnt with gaurd clause

--- Day_1-2.js
+++ Day_1-2.js
@@ -4,12 +4,14 @@ const numbers = testInput.map(input => Number(input))
 // const testInput = [1721, 979, 366, 299, 675, 1456]
 
 let answer
-
+const numberToMatch = 2020
 for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] >= numberToMatch) continue
   for (let j = 0; j < numbers.length; j++) {
+    if (numbers[i] + numbers[j] >= numberToMatch) continue
     for (let k = 0; k < numbers.length; k++) {
       const sumOfPair = numbers[i] + numbers[j] + numbers[k]
-      if (sumOfPair === 2020) {
+      if (sumOfPair === numberToMatch) {
         answer = numbers[i] * numbers[j] * numbers[k]
         break
       }
